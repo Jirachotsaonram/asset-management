9a6a9dfa1119b7b8bbbd6be358d693c7013e3b04
Jirachot Saonram <109726423+Jirachotsaonram@users.noreply.github.com>
2026-05-25 20:55:23 +0700

    version 0.9.1

--- unused_files/update_status.sql
+++
@@ -1,3 +0,0 @@
-UPDATE assets SET status = 'ใช้งาน' WHERE status = 'ใช้งานได้';
-UPDATE checks SET check_status = 'ใช้งาน' WHERE check_status = 'ใช้งานได้';
-UPDATE borrows SET return_condition = 'ใช้งาน' WHERE return_condition = 'ใช้งานได้';
